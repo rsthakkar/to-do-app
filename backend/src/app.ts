e31a3a0aacd544046ee7030c826cf72b08c7702b
rsthakkar <rsthakkar66@gmail.com>
2022-08-07 21:25:42 +0530

    added mongodb connection

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,15 @@
+require("dotenv").config();
+
 import express from 'express';
+import { MongoClient, ServerApiVersion }  from 'mongodb'; 
+
+
 const app = express();
 const port = 3000;
+
+const client = new MongoClient(process.env.MONGO_URI);
+client.connect().then(() => {console.log("connected")});
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
